a81992a79ff4a5a5993a3cb19fdb66c99a680f6d
AlicjaWl <a.wladyszewska@gmail.com>
2023-05-23 21:44:14 +0200

    Wczytywanie danych_jupyter_unseafe_speed

--- Wczytywanie_danych.py
+++ Wczytywanie_danych.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import requests
 
-glowna_sciezka_danych = '/home/student/'
-plik = glowna_sciezka_danych + "nypd-motor-vehicle-collisions.csv"
-collisions_data = pd.read_csv(plik)
-#collisions_data = pd.read_csv("data/nypd-motor-vehicle-collisions.csv")
-collisions_data
+nypd_df = pd.read_csv("/home/student/nypd-motor-vehicle-collisions.csv")
+nypd_df
 
+Unsafe_speed_selectes_data = nypd_df[
+    (nypd_df['CONTRIBUTING FACTOR VEHICLE 1'] == 'Unsafe Speed')
+    | (nypd_df['CONTRIBUTING FACTOR VEHICLE 2'] == 'Unsafe Speed')
+    | (nypd_df['CONTRIBUTING FACTOR VEHICLE 3'] == 'Unsafe Speed')
+    | (nypd_df['CONTRIBUTING FACTOR VEHICLE 4'] == 'Unsafe Speed')
+    | (nypd_df['CONTRIBUTING FACTOR VEHICLE 5'] == 'Unsafe Speed')]
+Unsafe_speed_selectes_data
+
+Unsafe_speed_selectes_data_columns = Unsafe_speed_selectes_data[['BOROUGH','NUMBER OF PERSONS INJURED','NUMBER OF PERSONS KILLED','NUMBER OF PEDESTRIANS INJURED','NUMBER OF PEDESTRIANS KILLED','NUMBER OF CYCLIST INJURED','NUMBER OF CYCLIST KILLED','NUMBER OF MOTORIST INJURED','NUMBER OF MOTORIST KILLED','CONTRIBUTING FACTOR VEHICLE 1','CONTRIBUTING FACTOR VEHICLE 2','CONTRIBUTING FACTOR VEHICLE 3','CONTRIBUTING FACTOR VEHICLE 4','CONTRIBUTING FACTOR VEHICLE 5']]
+Unsafe_speed_selectes_data_columns
 #ciezka_wynikowa = glowna_sciezka + "plik_wynikowy.csv"
